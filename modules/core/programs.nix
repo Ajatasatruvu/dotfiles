8ab3794dcbdc005a5718c612883becb845ea5e64
harsha <wwbad21@gmail.com>
2025-05-24 12:02:52 +0530

    add jmtpfs for mtp

--- modules/core/programs.nix
+++ modules/core/programs.nix
@@ -3,11 +3,13 @@
   programs = {
     hyprland.enable = true;
     firefox.enable = true;
+    fuse.userAllowOther = true;
   };
   environment.systemPackages = with pkgs; [
     brightnessctl
     eza
     git
+    jmtpfs
     kdePackages.okular
     playerctl
     wl-gammarelay-rs
